0d70f0443491b233d6dec6aa999e3c358efbdada
omkaras <52051534+omkaras@users.noreply.github.com>
2021-05-03 00:59:34 +0530

    Update oddeven.js

--- 4_Java_Script_Tutorials/oddeven.js
+++ 4_Java_Script_Tutorials/oddeven.js
@@ -1,10 +1,10 @@
 function oddeven(num) {
     var n;
-      n =  num % 2
-      if (n === 0) {
+      n =  num % 2 // you can declare and initialize variable in a same line
+      if (n === 0) { 
        console.log('Number is Even');
-      } else if (n === 1) {
+      } else if (n === 1) { /// no need of repeated if statement
        console.log('Number is Odd');
       }
-      else {console.log('Invalid Number');}
+      else {console.log('Invalid Number');} // use better checks to identify where input (num) is a number
       }
